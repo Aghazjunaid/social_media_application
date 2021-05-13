890b2ce9d270f5f6704622d1ce00b995d789d236
shadmaan <md.shadmaan@nexilis.com>
2021-05-13 13:00:18 +0530

    auth token

--- utils.js
+++ utils.js
@@ -2,7 +2,20 @@ module.exports = () => {
     const jwt = require('jsonwebtoken');
 
 
-
+    function authenticateToken(req, res, next) {
+        const token = req.headers['authorization'];
+        if (!token){
+            return res.status(401).send("Invalid authorization");
+        } else {
+            jwt.verify(token, "gsjkah35gsj546b5t", (error, user) => {
+                if (error){
+                    return res.status(400).send(error);
+                }
+                req.user = user;
+                next();
+            })
+        }
+    }
 
     function otpGenerator(){
         var otp = Math.random();
@@ -13,6 +26,6 @@ module.exports = () => {
 
 
     return{
-
+        authenticateToken
     }
 }
