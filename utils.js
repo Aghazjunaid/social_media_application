674390701820ec28dedf255325dece5405c4a985
shadmaan <md.shadmaan@nexilis.com>
2021-05-13 14:11:36 +0530

    dropbox api

--- utils.js
+++ utils.js
@@ -44,12 +44,46 @@ module.exports = () => {
             subject: "OTP for verification of your email",
             text: "OTP for account verification is "+otp+"", 
           });
-      }
+    }
+
+    function validateImage(file) {
+        return_value = {"name": "", "status": false}
+        try {
+            if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
+                return_value["name"]=file.originalname;
+                return_value["status"] = true
+            } else{
+                return_value["status"] = false
+            }
+        } catch (error) {
+          return_value["status"] = false
+        }
+        return return_value
+    }
+      
+    async function dropboxUpload(file){
+        let response = validateImage(file)
+        if(response.status) {
+            let newFilename = `${otpGenerator()}_${response.name}`
+            let contents = file.buffer;
+            let uploaded = await dbx.filesUpload({ path: `/images/${newFilename}`, contents})
+            if(uploaded.status === 200) {
+                var result = await dbx.sharingCreateSharedLink({path:`/images/${newFilename}`});
+                return result.result.url.split("dl")[0] + 'raw=1';
+            } else {
+                return null
+            }
+        }else {
+            return null
+        }
+    }
+
 
 
     return{
         authenticateToken,
         emailSend,
-        otpGenerator
+        otpGenerator,
+        dropboxUpload
     }
 }
