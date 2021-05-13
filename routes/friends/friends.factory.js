10e118dde06bc90e7c929ef0f184ffc7131805b5
shadmaan <md.shadmaan@nexilis.com>
2021-05-13 21:41:47 +0530

    remove friend api

--- routes/friends/friends.factory.js
+++ routes/friends/friends.factory.js
@@ -63,11 +63,35 @@ module.exports = ({
         res.json(return_response);
     }
 
+    //====================remove friend=============================================
+    async function removeFriend(req,res){
+        var return_response = { "status": null, "message": null, "data": {} } 
+        try {
+            const friend = await Friends.find({user:req.user.id});
+            var index = friend[0]._doc.friends.indexOf(req.params.id);
+            if (index > -1) {
+                friend[0]._doc.friends.splice(index, 1);
+            }
+            const opt = {
+                friends : friend[0]._doc.friends
+            }
+            const doc = await Friends.findOneAndUpdate({user:req.user.id}, opt, {new:true})
+            return_response.status = 200;
+            return_response.message = "Success";
+            return_response.data = doc;
+        } catch (error) {
+            return_response.status = 400;
+            return_response.message = String(error);
+        }
+        res.json(return_response);
+    }
+
+
     //=====================get all my friends========================================
     async function viewMyFriends(req,res){
         var return_response = { "status": null, "message": null, "data": {} } 
         try {
-            const doc = await Friends.find({user:req.user.id});
+            const friends = await Friends.find({user:req.user.id});
             return_response.status = 200;
             return_response.message = "Success";
             return_response.data = doc;
@@ -84,6 +108,7 @@ module.exports = ({
     return {
         sendFriendRequest,
         acceptFriendRequest,
+        removeFriend,
         viewMyFriends
     }
 }
