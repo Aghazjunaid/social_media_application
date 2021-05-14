acf55097a3059bc943e41e762072718685ec8c32
shadmaan <md.shadmaan@nexilis.com>
2021-05-14 14:03:37 +0530

    adding friends comments

--- routes/friends/friends.factory.js
+++ routes/friends/friends.factory.js
@@ -6,10 +6,15 @@ module.exports = ({
 }) => {
 
     //===============send friend request============================================
+    //This api will send friend request and those who send request his _id will be stored
+    //in the PendingFriends DB.
     async function sendFriendRequest(req,res){
         var return_response = { "status": null, "message": null, "data": {} } 
         try {
+            //First check any pending friend request is available or not
             const pendingFriends = await PendingFriends.findOne({user:req.user.id});
+            //If not any friend request available then stored in the first index of array
+            //by below code
             if (!pendingFriends){
                 let opt = {
                     "user": req.user.id,
@@ -20,6 +25,8 @@ module.exports = ({
                 return_response.status = 200;
                 return_response.message = "Friend request send successfully";
                 return_response.data = doc;
+            //If any prending friend request is present then push the new friend request
+            //to the last index of array by below code
             } else {
                 pendingFriends._doc.pendingFriends.push(req.params.id)
                 let doc = await pendingFriends.save();
@@ -35,10 +42,16 @@ module.exports = ({
     }
 
     //======================accept friend request=====================================
+    //This api will take user id in params whose friend request is going to accept
+    //In this once the friend request is accepted friend is added to both(login user and
+    //whose friend request is going to accept) friends list
+    //And then remove the friend request from pending friends
     async function acceptFriendRequest(req,res){
         var return_response = { "status": null, "message": null, "data": {} } 
         try {
+            //First check any friend is available or not in the friends DB
             const friends = await Friends.findOne({user:req.user.id});
+            //If no any friends are available then stored in the first index of array and save it
             if (!friends){
                 let opt = {
                     "user": req.user.id,
@@ -49,6 +62,8 @@ module.exports = ({
                 return_response.status = 200;
                 return_response.message = "Friend request accepted successfully";
                 return_response.data = doc;
+            //If any prending friend request is present then push the new friend request
+            //to the last index of array and save it
             } else {
                 friends._doc.friends.push(req.params.id)
                 let doc = await friends.save();
@@ -68,7 +83,9 @@ module.exports = ({
                 friend2._doc.friends.push(req.params.id)
                 await friend2.save();
             }
+            //search friend request in the pending friends by id
             const pendingFriends = await PendingFriends.find({user:req.user.id});
+            //After finding splice it from the array
             var index = pendingFriends[0]._doc.pendingFriends.indexOf(req.params.id);
             if (index > -1) {
                 pendingFriends[0]._doc.pendingFriends.splice(index, 1);
@@ -76,6 +93,7 @@ module.exports = ({
             const opt = {
                 pendingFriends : pendingFriends[0]._doc.pendingFriends
             }
+            //Then update the PendingFriends DB 
             await PendingFriends.findOneAndUpdate({user:req.user.id}, opt, {new:true})
         } catch (error) {
             return_response.status = 400;
@@ -85,10 +103,14 @@ module.exports = ({
     }
 
     //====================reject friend request=====================================
+    //This api will take user id in params whose friend request is going to reject
     async function rejectFriendRequest(req,res){
         var return_response = { "status": null, "message": null, "data": {} } 
         try {
+            //First check the pending request of user 
             const pendingFriends = await PendingFriends.find({user:req.user.id});
+            //Find the user's id which is going to reject by finding its index in array
+            //then splice it from  pendingFriends
             var index = pendingFriends[0]._doc.pendingFriends.indexOf(req.params.id);
             if (index > -1) {
                 pendingFriends[0]._doc.pendingFriends.splice(index, 1);
@@ -96,6 +118,7 @@ module.exports = ({
             const opt = {
                 pendingFriends : pendingFriends[0]._doc.pendingFriends
             }
+            //After removing update the PendingFriends DB
             const doc = await PendingFriends.findOneAndUpdate({user:req.user.id}, opt, {new:true})
             return_response.status = 200;
             return_response.message = "Success";
@@ -108,10 +131,13 @@ module.exports = ({
     }
 
     //====================remove friend=============================================
+    //This api will take friend's id in params which is need to remove from the friend list
     async function removeFriend(req,res){
         var return_response = { "status": null, "message": null, "data": {} } 
         try {
             const friend = await Friends.find({user:req.user.id});
+            //Find the user's id which is going to remove from the logged in user
+            //friend list by finding its index in array of friends list
             var index = friend[0]._doc.friends.indexOf(req.params.id);
             if (index > -1) {
                 friend[0]._doc.friends.splice(index, 1);
@@ -119,6 +145,7 @@ module.exports = ({
             const opt = {
                 friends : friend[0]._doc.friends
             }
+            //Then update the friends DB of user.
             const doc = await Friends.findOneAndUpdate({user:req.user.id}, opt, {new:true})
             return_response.status = 200;
             return_response.message = "Success";
@@ -132,6 +159,7 @@ module.exports = ({
 
 
     //=====================get all my friends========================================
+    //This api will get all the friends of logged in user
     async function viewMyFriends(req,res){
         var return_response = { "status": null, "message": null, "data": {} } 
         try {
@@ -148,6 +176,8 @@ module.exports = ({
 
 
     //====================view friends of friend=========================================
+    //This api will take friends id whose friend list you want to view
+    //Then show his friends list
     async function viewFriendsOfFriend(req,res){
         var return_response = { "status": null, "message": null, "data": {} } 
         try {
@@ -162,7 +192,7 @@ module.exports = ({
         res.json(return_response);
     }
 
-    
+
 
     return {
         sendFriendRequest,
